7256bc3827c5dff6bb8b6246cadb9fb0fd48b114
corradif1996 <71105340+corradif1996@users.noreply.github.com>
2020-09-11 20:02:55 -0300

    java fede

---
+++ src/clases_Fede/Existencias.java
@@ -0,0 +1,36 @@
+
+package clases_Fede;
+
+public class Existencias {
+  private String producto;
+    private int precio;
+    private int cantidad; 
+
+    public String getProducto() {
+        return producto;
+    }
+
+    public void setProducto(String producto) {
+        this.producto = producto;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+    public static void registroDeVenta (){
+    }
+    public static void existencias (){
+    }   
+}
---
+++ src/clases_Fede/Inventario.java
@@ -0,0 +1,20 @@
+
+package clases_Fede;
+
+public class Inventario {
+    
+private int numero_solicitud;
+
+    public int getNumero_solicitud() {
+        return numero_solicitud;
+    }
+
+    public void setNumero_solicitud(int numero_solicitud) {
+        this.numero_solicitud = numero_solicitud;
+    }
+    
+    public static void registroPedido(){
+    }    
+    public static void existencias (){
+    }
+}
