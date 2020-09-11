d7689359add05c8e0b8a521ddcbdad836668c9a4
ElsaMariaMolina <e.maria.molina.p@gmail.com>
2020-09-11 19:25:53 -0300

    Clases Venta y Proveedor-ElsaMaria

---
+++ src/Entidades/Provedor.java
@@ -0,0 +1,20 @@
+
+package Entidades;
+
+
+public class Provedor {
+    private String telfono;
+    private String cuit;
+    private String domicilio;
+    private Integer cantidad;
+    private Producto producto;
+    private String nombre;
+    
+    
+    private void VentaDirecta(){
+    }
+    
+    private void PedidosEspeciales(){
+    }
+        
+}
